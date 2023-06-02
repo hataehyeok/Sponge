427a61ac25114bcaccd687c4215ac26eaadd85d2
“hataehyeok” <hth021002@postech.ac.kr>
2023-06-02 09:53:57 +0000

    Finish assn7

--- libsponge/network_interface.cc
+++ libsponge/network_interface.cc
@@ -16,7 +16,7 @@ using namespace std;
 // You will need to add private members to the class declaration in `network_interface.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -36,31 +36,27 @@ void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Addres
     const uint32_t next_hop_ipv4 = next_hop.ipv4_numeric();
     auto dst_info = arp_table.find(next_hop_ipv4);
 
-    if (dst_info != arp_table.end()) {
+    auto time = arp_time_table.find(next_hop_ipv4);
+    if (time != arp_time_table.end() && current_time <= time->second.second + 5000)
+        return;
+
+    if (dst_info != arp_table.end() && current_time <= dst_info->second.second + 30000) {
         send_ethernet_frame(dst_info->second.first, EthernetHeader::TYPE_IPv4, dgram.serialize());
-    } else {
-        auto time = arp_time_table.find(next_hop_ipv4);
-        if (time != arp_time_table.end()) {
-            if (current_time < time->second.second + 5000) {
-                return;
-            }
-            send_arp(next_hop_ipv4, 1);
-            time->second.second = current_time;
-            return;
-        }
-        send_arp(next_hop_ipv4, 1);
-        arp_time_table.emplace(next_hop_ipv4, make_pair(dgram, current_time));
+        return;
     }
+
+    send_arp(next_hop_ipv4, 1);
+    arp_time_table.emplace(next_hop_ipv4, make_pair(dgram, current_time));
 }
 
 //! \param[in] frame the incoming Ethernet frame
 optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &frame) {
-    
     const auto &ether_header = frame.header();
-    
+
     if (ether_header.dst.empty()) {
         return nullopt;
     }
+
     if (ether_header.type == EthernetHeader::TYPE_IPv4 && ether_header.dst == _ethernet_address) {
         InternetDatagram _dgram;
         ParseResult parse_res = _dgram.parse(frame.payload());
@@ -71,14 +67,20 @@ optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &fra
         ARPMessage _msg;
         ParseResult parse_res = _msg.parse(frame.payload());
         if (parse_res == ParseResult::NoError) {
+            if (_msg.target_ip_address != _ip_address.ipv4_numeric())
+                return nullopt;
+
             uint32_t _get_ip = _msg.sender_ip_address;
             arp_table[_get_ip] = make_pair(_msg.sender_ethernet_address, current_time);
 
-            if (_msg.opcode == ARPMessage::OPCODE_REQUEST && _msg.target_ip_address == _ip_address.ipv4_numeric()) {
-                send_arp(_get_ip, 0);
-            } else if (_msg.opcode == ARPMessage::OPCODE_REPLY && _msg.target_ip_address == _ip_address.ipv4_numeric()) {
-                send_ethernet_frame(arp_table[_get_ip].first, EthernetHeader::TYPE_IPv4, arp_time_table[_get_ip].first.serialize());
+            auto sent_frame = arp_time_table.find(_get_ip);
+            if (sent_frame != arp_time_table.end()) {
+                send_ethernet_frame(
+                    arp_table[_get_ip].first, EthernetHeader::TYPE_IPv4, arp_time_table[_get_ip].first.serialize());
                 arp_time_table.erase(_get_ip);
+            }
+            if (_msg.opcode == ARPMessage::OPCODE_REQUEST) {
+                send_arp(_get_ip, 0);
             }
         }
     }
@@ -86,11 +88,11 @@ optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &fra
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void NetworkInterface::tick(const size_t ms_since_last_tick) { 
+void NetworkInterface::tick(const size_t ms_since_last_tick) {
     current_time += ms_since_last_tick;
 
     auto it = arp_table.begin();
-    while(it != arp_table.end()) {
+    while (it != arp_table.end()) {
         if (current_time > it->second.second + 30000) {
             it = arp_table.erase(it);
         } else {
--- writeups/assn7.md
+++ writeups/assn7.md
@@ -1,35 +1,42 @@
 Assignment 7 Writeup
 =============
 
-My name: [your name here]
+My name: Taehyeok Ha
 
-My POVIS ID: [your povis id here]
+My POVIS ID: hth021002
 
-My student ID (numeric): [your student id here]
+My student ID (numeric): 20210273
 
-My assignment partner's name: [your partner's name here]
+My assignment partner's name: Yoonho Kim
 
-My assignment partner's POVIS ID: [your partner's povis id here]
+My assignment partner's POVIS ID: hjyang093
 
-My assignment partner's ID (numeric): [your partner's student id here]
+My assignment partner's ID (numeric): 20200003
 
-This assignment took me about [n] hours to do (including the time on studying, designing, and writing the code).
+This assignment took me about [5] hours to do (including the time on studying, designing, and writing the code).
 
 If you used any part of best-submission codes, specify all the best-submission numbers that you used (e.g., 1, 2): []
 
 - **Caution**: If you have no idea about above best-submission item, please refer the Assignment PDF for detailed description.
 
 Solo portion:
-[]
+[
+    Successfully fintsh that start and end a conversation with another copy of file. When our tcp protocol transfer the file, we can watch the message of 'has been fully acknowledged'. Firstly I fail to transfer file due to IPv4Datagram::serialize: payload is wrong size. To analysis this error, I use 'ls -al /tmp | grep big' and checking transmitted file size. File is sended but not fully, which mean that sender and receiver implement well but in network interface, there is some bug.
+    I consider exception code, and revise exception process.
+]
 
 Group portion:
-[]
+[
+    Team mate is Yoonho Kim(hjyang093) and we successfully fintsh that start and end a conversation with another copy of file. When we check file's hash value, we can see same value.
+]
 
 Creative portion (optional):
 []
 
 Other remarks:
-[]
+[
+    After implement sha256sum /tmp/big.txt, sha256sum /tmp/big-received.txt command, we get same value like 9b74c4c1c79e0a3967d0d312f67485671b8da5119cb741fab0494050fa9d5a0f.
+]
 
 - Optional: I had unexpected difficulty with: [describe]
 
